81556a9ced2bf0e9496a820384c13833110293e0
srinivas Kambalapally <srinivas.Kambalapally111@gmail.com>
2018-06-15 14:00:40 +0530

    test1

--- src/test/java/demo/Test.java
+++ src/test/java/demo/Test.java
@@ -1,10 +1,14 @@
 package demo;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Test {
     @org.testng.annotations.Test
 	public void main() {
-		System.out.println("Test Case is Executed");
-
+      System.setProperty("webdriver.gecko.driver","F://geckodriver.exe");
+      WebDriver driver=new FirefoxDriver();
+      driver.manage().window().maximize();
 	}
 
 }
