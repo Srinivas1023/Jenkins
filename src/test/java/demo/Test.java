5f6e7970db81550db3c592f59c8ac575d2e2a8a8
srinivas Kambalapally <srinivas.Kambalapally111@gmail.com>
2018-06-15 14:06:41 +0530

    test111

--- src/test/java/demo/Test.java
+++ src/test/java/demo/Test.java
@@ -1,12 +1,18 @@
 package demo;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class Test {
     @org.testng.annotations.Test
 	public void main() {
       System.setProperty("webdriver.gecko.driver","F://geckodriver.exe");
+      DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+      capabilities.setCapability("marionette", true);
+      capabilities.setBrowserName("firefox");
+      capabilities.setPlatform(Platform.WINDOWS);
       WebDriver driver=new FirefoxDriver();
       driver.manage().window().maximize();
 	}
