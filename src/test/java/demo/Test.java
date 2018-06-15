98c9b507060e55b6d61423391ab0596a19b5a35e
srinivas Kambalapally <srinivas.Kambalapally111@gmail.com>
2018-06-15 14:20:59 +0530

    rr

--- src/test/java/demo/Test.java
+++ src/test/java/demo/Test.java
@@ -8,13 +8,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class Test {
     @org.testng.annotations.Test
 	public void main() {
-      System.setProperty("webdriver.chrome.driver","F://chromedriver.exe");
+    /*  System.setProperty("webdriver.chrome.driver","F://chromedriver.exe");
       DesiredCapabilities capabilities = DesiredCapabilities.firefox();
       capabilities.setCapability("marionette", true);
       capabilities.setBrowserName("chrome");
       capabilities.setPlatform(Platform.WINDOWS);
       WebDriver driver=new ChromeDriver();
-      driver.manage().window().maximize();
+      driver.manage().window().maximize();*/
+    	System.out.println("test");
 	}
 
 }
